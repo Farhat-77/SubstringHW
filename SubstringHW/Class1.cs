abe9f5622dbeecf3f0d458e66168edeb3b38fa6b
Bola_sw25@student.itstep.kz <Bola_sw25@student.itstep.kz>
2021-02-21 21:36:49 +0600

    Добавьте файлы проекта.

---
+++ SubstringHW/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SubstringHW
+{
+    public class AttendanceReport : BaseBean
+    {
+        public int Id { get; set; }
+        public int serialNo { get; set; }
+        public string aadharCard { get; set; }
+        public string employeeName { get; set; }
+    }
+
+    public class BaseBean
+    {
+    }
+}
---
+++ SubstringHW/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SubstringHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String value = "This is a string.";
+            int startIndex = 5;
+            int length = 2;
+            String substring = value.Substring(startIndex, length);
+            Console.WriteLine(substring);
+        }
+    }
+}
